a5ee6c3740e1f20710a1c3d38f7ed67501b799d2
7flash <7flash@users.noreply.github.com>
2026-07-14 08:31:26 +0800

    auto detect port

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -1,5 +1,5 @@
 import { serve } from "tradjs/web";
 
-const port = Number(process.env.PORT ?? 3000);
+const port = Number(process.env.BUN_PORT ?? null);
 await serve({ appDir: "./app", port });
 await new Promise(() => {});
